81566dc788756a2e732159b43b63c6991363d0d8
Zhihao Yu <akira.yzh@gmail.com>
2020-01-17 22:40:42 +0800

    changed

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 let oLottery = document.querySelector('.sqr_lottery');
 let oBtn = oLottery.querySelector('.btn');
-let aPrizes = ['爱爱', '亲亲', '抱抱', '打打', '锤锤', '哈哈'];
+let aPrizes = ['爱爱', '亲亲', '抱抱', '打打', '锤锤', '哈哈', '摸摸', '嘿嘿'];
+let sqrs = document.querySelectorAll('.sqr[id^=_]');
 
-//Insert prizes
-aPrizes.forEach((val, index) => {
-    oLottery.querySelector('#_' + index).innerHTML = val;
-})
+render();
 
 // Click the button
 oBtn.addEventListener('click', run);
 
 function run(){
+    if(aPrizes.length==0){
+        alert('选项已经为空');
+        return;
+    }
     oBtn.removeEventListener('click', run);
-    aPrizes.forEach((val, index) => {
-        oLottery.querySelector('#_' + index).classList.remove('active');
-    });
+   
     oLottery.querySelector('#_0').classList.add('active');
     let bingo = rdm(0, aPrizes.length - 1);
     let count = total = 8 * 10 + bingo;
@@ -31,8 +31,8 @@ function rdm(min, max) {
 //Use sin() curve to calculate interval of setInterval
 function getInterval(t) {
     let y = Math.round(Math.sin(Math.PI * t) * 100) / 100;
-    let base = 300;
-    let changale = 270;
+    let base = 400;
+    let changale = 370;
     // console.log(y);
 
     return base - y * changale;
@@ -59,10 +59,33 @@ function _switch(count, total, now_index = 0, last_index = 0) {
             }else{
                 
                 alert('恭喜你,你中了 ' + aPrizes[total%8] + '!');
+                aPrizes = aPrizes.filter((val, index)=>{
+                    if(index!=total%8){
+                        return true;
+                    }
+                })
+                render()
+                sqrs.forEach((val) => {
+                    val.classList.remove('active');
+                });
+                
                 oBtn.addEventListener('click', run);
             }
 
         }, getInterval(--count / total));
 
 
+}
+
+//Insert prizes
+function render() {
+    
+    sqrs.forEach((div)=>{
+        div.innerHTML='';
+    })
+    console.log(sqrs);
+    
+    aPrizes.forEach((val, index) => {
+        oLottery.querySelector('#_' + index).innerHTML = val;
+    })
 }
